68d92af78f5b332596c3ab5fbe94fcb014faf12f
Maria Mills <maria@mariamills.org>
2022-10-15 21:18:39 -0400

    Added comments

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const paginationBtns = document.querySelectorAll(".slider__pagination");
 const slideItems = document.querySelectorAll(".slider__item");
 let slide = document.querySelector(".slider");
+
+// Currently using this variable for my timedSlide function to iterate through the different slides
 let i = 1;
+//Change this value to change the automated delay, currently set to 12 seconds
 const delay = 12000;
 
 function activeCheck(list) {
@@ -39,6 +42,7 @@ paginationBtns.forEach(function (btn, index) {
   });
 });
 
+// Calling the timedSlide function on window load
 window.addEventListener("load", function () {
   timeSlide();
 });
